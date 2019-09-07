0bf139e3ecd351d97c78c3a5353da7928b7e2c42
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-09-07 15:56:11 -0700

    initial php commit

---
+++ pepjoeCV.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_POST['submit'])){
+   $title = $_POST['title'];
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+   
+   echo "Name:".$_POST['name'] . '<br>';
+   echo "Email:".$_POST['email'] . '<br>';
+   echo "Title:".$_POST['title'] . '<br>';
+   echo "Message:".$_POST['message'] . '<br>';
+}
+
+function pre_r( $array) {
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+?>
